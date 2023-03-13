2111eb2561b9257f6ac7e6ee394192a7334a5967
Ivo Yordanov <ivosvetcavicata@gmail.com>
2023-03-13 02:38:43 +0100

    Fixed menu opening on mobile

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,6 +1,21 @@
-$(document).ready(main);
-function main() {
-    $('.header_menu').click(function(){
-        $('nav').toggle();
-    });
-}
+var w = window.innerWidth;
+function menuToggle() {
+    var links = document.getElementsByClassName("header_nav-ul-li");
+    for (var i=0; i<links.length;i++){
+        if (links[i].style.display === "block" && w < 800) {
+            links[i].style.display = "none";
+          } else {
+            links[i].style.display = "block";
+          }
+    }
+  }
+
+  function menuDisplay() {
+    if (w >= 800){
+        for (var i=0; i<links.length;i++){ 
+         links[i].style.display = "flex";
+        }
+     }
+  }
+
+  window.addEventListener('resize', menuDisplay());
